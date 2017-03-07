50450adc6b53981263d130c4c6516e53027c0b9a
lijingyuanuk <380867948@qq.com>
2017-03-07 11:58:18 +0000

    -delete error -solve git confict

--- dealcashflows/views.py
+++ dealcashflows/views.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
 import StringIO
-<<<<<<< Updated upstream
-
-=======
-import BeautifulSoup
->>>>>>> Stashed changes
 import time
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
